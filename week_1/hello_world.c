31ba49d599a7bbf207c715d4fbbc935dec3ef820
Max <maxdavisonnz@gmail.com>
2025-09-20 15:24:51 +1000

    T3 Setup

--- week_1/hello_world.c
+++ week_1/hello_world.c
@@ -4,9 +4,7 @@
 
 int main(void) {
     // This prints "Hello, World!" to the terminal, and the next terminal
-    // command is printed on the line below it, instead of on the same
-    // line.
-    // What does this imply about what '\n' does?
+    // command is printed on the line below it, instead of on the same line.
     printf("Hello, World!\n");
     
     return 0;
