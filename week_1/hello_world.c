ba6d635d3eeaab43af76b810785f78757ed2100e
Max <maxdavisonnz@gmail.com>
2026-02-21 23:05:33 +1100

    Week 1 Code

---
+++ week_1/hello_world.c
@@ -0,0 +1,11 @@
+// Basic Hello, World! program
+
+#include <stdio.h>
+
+int main(void) {
+    // This prints "Hello, World!" to the terminal, and the next terminal
+    // command is printed on the line below it, instead of on the same line.
+    printf("Hello, World!\n");
+    
+    return 0;
+}
